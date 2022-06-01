68a963e14baea9f7c1c780ba079719d824906743
markporo <markporo@yahoo.com>
2022-06-01 14:46:11 -0400

    Updates

--- src/components/ErrorPage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ErrorPage() {
-  return (
-    <div className="text-2xl text-rose-400">
-        Oops! Looks like that Page does not exist. 
-    </div>
-  )
-}
